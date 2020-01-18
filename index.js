068fa1c53bf3c9ec0a216054f7df6f8b172cbce5
github username <github email address>
2020-01-17 22:46:29 -0700

    finished reduce lab

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
-function totalBatteries(batterBatches){
-    let sum = batterBatches.reduce((total, num) => {
+let totalBatteries = batteryBatches.reduce((total, num) => {
         return num + total
     }, 0)
-    return sum
-}   
+ 
+
